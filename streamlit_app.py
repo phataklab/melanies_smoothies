cb2b8f422b1c3b6f22105f05c0f57117727cbaba
phataklab <63642875+phataklab@users.noreply.github.com>
2025-07-14 15:38:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=True)
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=true)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
    
 
 ingredients_list = st.multiselect(
