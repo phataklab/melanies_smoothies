4fb12c0d4feaed1892ea49e4c4df55938f58d3ad
phataklab <63642875+phataklab@users.noreply.github.com>
2025-07-14 15:35:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,10 +19,9 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
-
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.jason())
-    
+   
 
 ingredients_list = st.multiselect(
     'Choose up to 5 Ingredients:' 
